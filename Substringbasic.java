c65acaa88813ab1aab40dec014410f56f3c1a9bd
JJohnR08 <rassijos000@rocori.k12.mn.us>
2026-03-24 16:15:15 +0000

    antidisestablishmentarianism

---
+++ Substringbasic.java
@@ -0,0 +1,9 @@
+// This program demonstrates substring() with inclusive/exclusive indices.
+public class Substringbasic {
+public static void main(String[] args) {
+String word = "antidisestablishmentarianism";
+System.out.println(word.substring(0, 3)); // &quot;com&quot;
+System.out.println(word.substring(3)); // &quot;puter&quot;
+System.out.println(word.substring(0, 28)); // &quot;omp&quot;
+}
+}
